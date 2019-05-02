e2e50911e58ceb0e47d3e647680750a3bc70f9f1
NumPoet <bonekob2rt@gmail.com>
2019-05-02 06:21:22 -0500

    actualizando grafica mensual

--- models/Responsables.php
+++ models/Responsables.php
@@ -262,7 +262,8 @@ class Responsables {
     $result1 = $this->db->query($mesAbril);
 
     // $dia2="SELECT count(*) AS fecha FROM Usuario WHERE fechaDeRegistro LIKE '%2019-04-02%'";
-    // $result2 = $this->db->query($dia2);
+    $mesMayo="SELECT count(*) AS fecha FROM Usuario U1, UsuariosPorPilar U2 WHERE U1.idUsuarios = U2.Usuario_idUsuarios AND U2.Pilares_idPilares = '$lcpPilarId' AND U1.fechaDeregistro like '%2019-05%'";
+    $result2 = $this->db->query($mesMayo);
 
     // $dia3="SELECT count(*) AS fecha FROM Usuario WHERE fechaDeRegistro LIKE '%2019-04-03%'";
     // $result3 = $this->db->query($dia3);
@@ -334,9 +335,9 @@ class Responsables {
     foreach ($result1 as $row) {
     $data[] = $row;
     }
-    // foreach ($result2 as $row) {
-    // array_push($data, $row);
-    // }
+    foreach ($result2 as $row) {
+    array_push($data, $row);
+    }
     // foreach ($result3 as $row) {
     // array_push($data, $row);
     // }
