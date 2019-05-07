c382821c2e5f66ed72bdb7897d99038b551a58da
sergio <cbi2143044162@titlani.uam.mx>
2019-05-07 11:12:52 -0500

    credencial de produccion-test

--- config/parameters.php
+++ config/parameters.php
@@ -1,6 +1,6 @@
 <?php
-// define("URL", "http://187.216.164.109/CRUD-LCP/");
-define("URL", "http://localhost/Pilares/CRUD-LCP/");
+define("URL", "http://187.216.164.109/CRUD-LCP/");
+// define("URL", "http://localhost/Pilares/CRUD-LCP/");
 define("controller_default", "CrudController");
 define("action_default", "index");
 ?>
