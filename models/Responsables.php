38271ca2d3bfecd9851a4e041bcdf9c705d6d440
Sergio Negrete <bonekob2rt@gmail.com>
2019-07-03 10:59:05 -0500

    Aumentando parametros de mes  en  gráfica de ususarios por mes

--- models/Responsables.php
+++ models/Responsables.php
@@ -270,7 +270,8 @@ class Responsables {
     $result3 = $this->db->query($mesJunio);
 
     // $dia4="SELECT count(*) AS fecha FROM Usuario WHERE fechaDeRegistro LIKE '%2019-04-04%'";
-    // $result4 = $this->db->query($dia4);
+    $mesJulio="SELECT count(*) AS fecha FROM Usuario U1, UsuariosPorPilar U2 WHERE U1.idUsuarios = U2.Usuario_idUsuarios AND U2.Pilares_idPilares = '$lcpPilarId' AND U1.fechaDeregistro like '%2019-07%'";
+    $result4 = $this->db->query($mesJulio);
 
     // $dia5="SELECT count(*) AS fecha FROM Usuario WHERE fechaDeRegistro LIKE '%2019-04-05%'";
     // $result5 = $this->db->query($dia5);
@@ -342,9 +343,9 @@ class Responsables {
     foreach ($result3 as $row) {
     array_push($data, $row);
     }
-    // foreach ($result4 as $row) {
-    // array_push($data, $row);
-    // }
+    foreach ($result4 as $row) {
+    array_push($data, $row);
+    }
     // foreach ($result5 as $row) {
     // array_push($data, $row);
     // }
