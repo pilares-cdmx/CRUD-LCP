1899ae62b16e4d565e6d029e7f988d013054aa16
sergio <bonekob2rt@gmail.com>
2019-08-29 15:05:53 -0500

    Agregando  por segmentos por genero en usuarios por actividad

--- views/crud/indexJUD.php
+++ views/crud/indexJUD.php
@@ -15627,7 +15627,7 @@
         </div>
 
         <div class="row">
-          <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="col-xl-6 col-sm-6 mb-3">
             <div class="card text-white bg-success o-hidden h-100">
               <div class="card-body">
                 <div class="card-body-icon">
@@ -16132,7 +16132,7 @@
         </div>
 
         <div class="row">
-          <div class="col-xl-3 col-sm-6 mb-3">
+          <div class="col-xl-6 col-sm-6 mb-3">
             <div class="card text-white bg-danger o-hidden h-100">
               <div class="card-body">
                 <div class="card-body-icon">
