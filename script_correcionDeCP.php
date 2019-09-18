5353e9035326183d5e9ecb22016c91a9a6641cee
conejo <conejo@ome.calli>
2019-09-18 12:20:24 -0500

    depurando codigos postales  de  becas.  agregando  cero

--- script_correcionDeCP.php
+++ script_correcionDeCP.php
@@ -10,7 +10,7 @@ $con = mysqli_connect('localhost', 'francisco', 'tu_contrasena', 'pilaresDB');
     mysqli_query($con, "SET NAMES 'utf8mb4'");
 
 
-    $sql="SELECT COUNT(*) AS userPorActividad FROM ActividadesPorUsuario A1, UsuariosPorPilar U1 WHERE A1.Usuario_idUsuarios = U1.Usuario_idUsuarios and A1.Actividades_TiposActividades_idTiposActividades = '1' and U1.Pilares_idPilares = '$idPilarLCP'";
+    $sql="SELECT COUNT(*) FROM Usuario;";
     // $sql="SELECT count(*) AS userPorActividad FROM ActividadesPorUsuario WHERE Actividades_TiposActividades_idTiposActividades = '1'";
     $totalesCultura = mysqli_query($con, $sql);
     //var_dump($totalesCultura);
