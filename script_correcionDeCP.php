12b1928643f36bf2c2905bb801b9cd5e334b03a9
conejo <conejo@ome.calli>
2019-09-18 12:38:44 -0500

    depurando codigos postales  de  becas.  agregando  cero

--- script_correcionDeCP.php
+++ script_correcionDeCP.php
@@ -22,6 +22,8 @@ $con = mysqli_connect('localhost', 'francisco', 'tu_contrasena', 'pilaresDB');
     //var_dump($totalesCPBecas);
     $cpSucio = mysqli_fetch_array($totalesCPBecas);
 
-  echo($cpSucio);
+    for ($i=0; $i < strlen ($cpSucio) ; $i++) { 
+        echo($cpSucio[$i]);
+    }
 
 ?>
