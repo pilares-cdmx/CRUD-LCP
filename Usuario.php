e467e9c85df1d6973a28da347e8781754f999599
sergio <bonekob2rt@gmail.com>
2019-10-08 16:09:25 -0500

    realizando script php para corregir error de formato scotiabank

--- Usuario.php
+++ Usuario.php
@@ -3498,14 +3498,14 @@ $Usuario = array('96CR010',
     '2111NL07',
     '189MS09'
 );
-var_dump(count($Usuario));die;
+// var_dump(count($Usuario));die;
 $curpBecarios;
 
 for ($i=0; $i < count($Usuario); $i++) { 
-  $sql="SELECT curp FROM Ususario WHERE folio = '$Usuario[$i]'";
+  $sql="SELECT curp FROM Usuario WHERE folio = '$Usuario[$i]'";
   $curpCorrectoBecas = mysqli_query($con, $sql); 
   array_push($curpBecarios, $curpCorrectoBecas);
 }
-
+var_dump($curpBecarios);die;
 echo($curpBecarios);
 ?>
